75c26b7450a3462a0ac18b48600c0c4e97d4b62c
Aniket Bista <aniketbista@gmail.com>
2021-09-07 01:49:28 +0545

    First Commit

---
+++ Test_OrangeHRMLogin.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import unittest
+import HtmlTestRunner
+
+class OrangeHRMLogin(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path='C:\\WebDrivers\\chromedriver_win32\\chromedriver.exe')
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        print('OrangeHRM Login Test Completed ... ')
+
+    def test_homePageTitle(self):
+        self.driver.get('https://opensource-demo.orangehrmlive.com/')
+        self.assertEqual('OrangeHRM', self.driver.title, 'Webpage titles are not same ')
+
+    def test_login(self):
+        self.driver.find_element_by_id('txtUsername').send_keys('Admin')
+        self.driver.find_element_by_id('txtPassword').send_keys('admin123')
+        self.driver.find_element_by_id('btnLogin').click()
+        self.assertEqual('OrangeHRM', self.driver.title, 'Webpage titles are not same ')
+
+if __name__ == "__main__":
+    unittest.main()
